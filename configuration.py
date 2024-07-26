a7cec5710772beb1af76b7216d0b3d90020a8731
Alan Rodriguez <rodrigueza27@uabc.edu.mx>
2024-07-26 09:32:08 -0700

    Segunda entrega link actualizado

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-3a760db8-f9e3-47e9-9d39-0286e0c0eff1.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-0abbd6b3-51cd-452e-9329-bb20adb68a7d.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
